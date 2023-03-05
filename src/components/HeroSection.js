dfe5891509c472088c8534d8c4e01c664fe0b3da
Akinniran Oke <oakinniran@hremsoft.com>
2023-03-05 12:52:37 +0100

    new update howtovideos2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css'
 import { Button } from './Button';
 
 function HeroSection() {
-  let url="https://applications.oes.com.ng/OESWebApp/erregister.do";
+  let url="https://applications.oes.com.ng/OESWebApp/register.do";
   let url1="https://applications.oes.com.ng/OESWebApp/createschoolout.do";
   let url2="https://applications.oes.com.ng/OESWebApp/";
 
@@ -19,25 +19,25 @@ function HeroSection() {
 
        <div className="hero-btns">
        <Button 
-          url={url1}
+          url={url}
          className='btns' 
          buttonStyle='btn--outline'
          buttonSize='btn--large'>
-           Register as a Student
+           Register as a Student - Past Question
          </Button>
        <Button 
           url={url1}
          className='btns' 
          buttonStyle='btn--outline'
          buttonSize='btn--large'>
-           Register a School
+           Register a School -  School Management System
          </Button>
          <Button 
          url={url2}
          className='btns' 
          buttonStyle='btn--primary'
          buttonSize='btn--large'>
-           Creating Lecture <i className='far fa-play-circle'></i>
+           Creating Lecture - As a Teacher
          </Button>
        </div>
 
